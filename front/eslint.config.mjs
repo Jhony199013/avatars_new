c05c4db0dd56af54b819695cec0fbabe4d544a17
Jhony199013 <7733123@mail.ru>
2025-11-29 23:49:46 +0300

    Clean repo + applied gitignore

--- front/eslint.config.mjs
+++ front/eslint.config.mjs
@@ -5,6 +5,12 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
